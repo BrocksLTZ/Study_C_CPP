93f798fd6866a7f93866ef9df62a6cc680185e86
BrocksLTZ <vsmalakhov000@gmail.com>
2022-02-13 17:41:21 +0300

    1

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,10 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdio.h>
 
 size_t timer_from(unsigned char from);
 // TODO: Implement `power of` function
 // int custom_pow(int base, int power);
 
-#endif //UTILS_H
+#endif //  UTILS_H
 
--- project/src/utils.c
+++ project/src/utils.c
@@ -1,10 +1,10 @@
 #include "utils.h"
 
-size_t timer_from(unsigned char from) {
+size_t timer_from(size_t from) {
     size_t counter;
-    for (unsigned char i = from; i >= 0; --i) {
+    for (size_t i = from; i >= 0; --i) {
         ++counter;
-        printf(i);
+        printf("%ld\n", i);
     }
     return counter;
 }
